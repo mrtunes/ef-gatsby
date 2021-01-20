39d5d115c9a9c67d9128dcbfc945c943b5fa2d94
Elliott Fienberg <elliottfienberg@Elliotts-MacBook-Air.local>
2021-01-20 09:23:52 -0500

    added sc to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,8 +64,12 @@ const IndexPage = ({ data }) => {
           </p>
           <h5>Latest Releases</h5>
           <Row>
-
-
+          
+<Col>
+<iframe width="100%" height="250" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/967318108&color=%23272232&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe> <br />
+</Col>
+</Row>
+<Row>
 
             {latestReleases.map(release => (
               <Col xs={6} md={4} key={release.uri} className="mt-2 mb-3">
